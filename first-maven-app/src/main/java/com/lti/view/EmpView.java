d6c39c732664b1114e1914e85c1a8f5cbf30f119
Dhruv2211 <dhruvpujara06@gmail.com>
2021-03-25 12:37:48 +0530

    second commit

--- first-maven-app/src/main/java/com/lti/view/EmpView.java
+++ first-maven-app/src/main/java/com/lti/view/EmpView.java
@@ -6,8 +6,8 @@ public class EmpView {
 	
 	public static void main (String []args) {
 		Employee emp = new Employee();
-		emp.setEmpId(1001);
-		emp.setEmpName("Harry");
+		emp.setEmpId(1002);
+		emp.setEmpName("John");
 		
 		
 		System.out.println("Id: "+emp.getEmpId());
